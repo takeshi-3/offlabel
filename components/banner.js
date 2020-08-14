27147e24ecbb0bca47acab6052f0459f11a8580d
Takeshi <g.jem.beleeem8000@gmail.com>
2020-08-14 18:46:02 +0900

    add contact link

--- components/banner.js
+++ components/banner.js
@@ -1,9 +1,12 @@
 import styles from './styles/banner.module.scss';
 import PlayArrowRoundedIcon from '@material-ui/icons/PlayArrowRounded';
 
+import Link from 'next/link';
+
 const Banner = ({title, exp}) => {
     return (
         <div className={styles.wrapper}>
+        <Link href="/contact/"><a>
             <div className={styles.wrapper_back}>
                 <h1 className={styles.title}>
                     <PlayArrowRoundedIcon color="primary" style={{fontSize: 30}} />
@@ -11,6 +14,7 @@ const Banner = ({title, exp}) => {
                 </h1>
                 <p className={styles.exp}>{exp}</p>
             </div>
+        </a></Link>
         </div>
     );
 };
